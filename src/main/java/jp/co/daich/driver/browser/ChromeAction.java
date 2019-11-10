aed467c88b52eb2864dcf37da983e4c16d16ed3f
daich <fst-qst.w-it-h@docomo.ne.jp>
2019-11-10 06:26:08 +0000

    20191110 MOD take screenshot while scrolled only Chrome

--- src/main/java/jp/co/daich/driver/browser/ChromeAction.java
+++ src/main/java/jp/co/daich/driver/browser/ChromeAction.java
@@ -11,6 +11,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
 import jp.co.daich.constants.properNoun.WINDOWS;
+import jp.co.daich.driver.LonelyMyDriver;
 import jp.co.daich.manage.evi.EviSeqManager;
 import jp.co.daich.util.logger.MyLogger;
 import org.openqa.selenium.OutputType;
@@ -24,11 +25,32 @@ import org.openqa.selenium.WebDriver;
 public class ChromeAction implements BrowserDependentAction {
 
     /**
-     * 
-     * @param imgStorePath 
+     * スクロールで位置が変わらなくなるまで繰り返しスクリーンショットを保存する
+     *
+     * @param imgStorePath
      */
     @Override
     public void getScreenShot(WebDriver driver, String imgStorePath) {
+        // 直近ブラウザ表示のY位置 ※初期値を-1にすることで初回のisScrolledをtrueにしている
+        long latestViewTopHeight = -1;
+        long nowViewTopHeight = 0;
+
+        // ブラウザY座標が高くなり続ける間はスクリーンショットを保存する
+        while (nowViewTopHeight > latestViewTopHeight) {
+            takeCapture(driver, imgStorePath);
+            latestViewTopHeight = nowViewTopHeight;
+            // ウィンドウ高さ分だけスクロール
+            nowViewTopHeight = scroll(driver);
+        }
+    }
+
+    /**
+     *
+     * @param driver
+     * @param imgStorePath
+     */
+    private void takeCapture(WebDriver driver, String imgStorePath) {
+
         // webdriverで撮った一時スクショファイル
         File sFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         Date date = new Date();
@@ -50,4 +72,18 @@ public class ChromeAction implements BrowserDependentAction {
         }
     }
 
+    /**
+     * ブラウザの高さ文だけスクロールする
+     *
+     * @param driver
+     * @return スクロール後の高さ
+     */
+    private long scroll(WebDriver driver) {
+        // scrollByの第一引数：x座標の移動距離
+        // scrollByの第二引数：y座標の移動距離
+        LonelyMyDriver.operate().executeJavaScript("scrollBy (0 , "
+                + LonelyMyDriver.operate().executeJavaScript("return window.innerHeight")
+                + ")");
+        return ((Number) LonelyMyDriver.operate().executeJavaScript("return window.pageYOffset")).longValue();
+    }
 }
